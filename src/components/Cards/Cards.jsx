1fd36a33427f7dcbe0a32ece27c438444948f922
Rebeca Pacheco <rbkgarner@gmail.com>
2020-06-24 15:50:45 -0300

    grid

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -12,7 +12,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
 
   return (
     <div className={styles.container}>
-      <Grid container spacing={3} justify="center">
+      <Grid container spacing={2} justify="center">
         <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.confirmed)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Casos Confirmados</Typography>
